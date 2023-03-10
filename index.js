125928f182d343b161ce8aa6a171ae028dfe4458
Nour Jandali <nourmshjan@gmail.com>
2023-03-11 01:11:52 +0200

    Remove console logs

--- index.js
+++ index.js
@@ -3,20 +3,15 @@ const { App } = require("@slack/bolt");
 require("dotenv").config();
 
 const TRELLO_KEY = process.env.TRELLO_KEY;
-console.log(TRELLO_KEY);
 const TRELLO_TOKEN = process.env.TRELLO_TOKEN;
-console.log(TRELLO_TOKEN);
 const app = new App({
   token: process.env.SLACK_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
-console.log(app);
 
 var trello = new Trello(TRELLO_KEY, TRELLO_TOKEN);
-console.log(trello);
 
 app.message(":trello:ADD", async ({ message, say }) => {
-  console.log("detected");
   const cardTitle = message.text.split('-t "')[1].split('"')[0];
   const cardDescription = message.text.split('-d "')[1].split('"')[0];
   trello.addCard(
@@ -35,7 +30,6 @@ app.message(":trello:ADD", async ({ message, say }) => {
 });
 
 (async () => {
-  await app.start(process.env.PORT);
-  console.log(process.env.PORT);
-  console.log("⚡️ Bolt app is running!");
+  await app.start(process.env.PORT || 3000);
+  console.log(`⚡️ Bolt app is running on port: ${process.env.PORT}`);
 })();
