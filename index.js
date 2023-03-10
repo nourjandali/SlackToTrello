1270d5b24f872600e633741d73a9544ae10fece3
Nour Jandali <nourmshjan@gmail.com>
2023-03-11 00:17:08 +0200

    Project init

---
+++ index.js
@@ -0,0 +1,37 @@
+const Trello = require("trello");
+const { App } = require("@slack/bolt");
+require("dotenv").config();
+
+const TRELLO_KEY = process.env.TRELLO_KEY;
+const TRELLO_TOKEN = process.env.TRELLO_TOKEN;
+
+const app = new App({
+  token: process.env.SLACK_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
+var trello = new Trello(TRELLO_KEY, TRELLO_TOKEN);
+
+app.message(":trello:ADD", async ({ message, say }) => {
+  const cardTitle = message.text.split('-t "')[1].split('"')[0];
+  const cardDescription = message.text.split('-d "')[1].split('"')[0];
+  trello.addCard(
+    cardTitle,
+    cardDescription,
+    "640ba599d219c4002fcf3117",
+    function (error, trelloCard) {
+      if (error) {
+        console.log("Could not add card:", error);
+      } else {
+        console.log("Added card:", trelloCard);
+      }
+    }
+  );
+  await say(`Card "${cardTitle}" created!`);
+});
+
+(async () => {
+  await app.start(process.env.PORT || 3000);
+
+  console.log("⚡️ Bolt app is running!");
+})();
